a77059d7b6b5d9959188aa6263ceeba7e80d2456
Jake Finn <lof.steven@gmail.com>
2022-02-17 20:11:59 -0600

    Make starting files

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS ecommerce_db;
+CREATE DATABASE ecommerce_db;
+
+CREATE TABLE users (
+  id INT NOT NULL,
+  username VARCHAR(30) NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  created_at TIMESTAMP,
+  modified_at TIMESTAMP,
+  password TEXT,
+);
