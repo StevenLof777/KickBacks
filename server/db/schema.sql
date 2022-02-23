7022044de676e483f1f3b50ec9479dc2a8fcfb19
Jake Finn <lof.steven@gmail.com>
2022-02-22 21:04:51 -0600

    Update schema

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,13 +1,35 @@
-DROP DATABASE IF EXISTS ecommerce_db;
-CREATE DATABASE ecommerce_db;
+DROP DATABASE IF EXISTS shoe_store_db;
+CREATE DATABASE shoe_store_db;
+USE shoe_store_db;
 
+DROP TABLE users;
 CREATE TABLE users (
-  id INT NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  zip_code INT(5) NOT NULL,
+  created_at TIMESTAMP,
+  modified_at TIMESTAMP,
+  -- How to validate
+  email VARCHAR(30) NOT NULL UNIQUE,
+  password VARCHAR(30) NOT NULL,
+  primary key(id)
+);
+SELECT * FROM users;
+
+CREATE TABLE shoes (
+  id INT AUTO_INCREMENT NOT NULL,
   username VARCHAR(30) NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   created_at TIMESTAMP,
   modified_at TIMESTAMP,
-  password TEXT,
+  password TEXT NOT NULL
 );
 
+CREATE TABLE category (
+  id INT AUTO_INCREMENT NOT NULL,
+  username VARCHAR(30) NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+);
