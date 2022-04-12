0f2f8c52668dc372f8bcbf70819fe2b3d492385b
Jake Finn <lof.steven@gmail.com>
2022-04-12 00:13:10 -0400

    Add add to cart button for product page. Use helmet to change title of page based to selected product

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,9 +1,10 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useReducer } from 'react';
 import axios from 'axios';
-import { Row, Col, ListGroup, Badge, Container } from 'react-bootstrap';
+import { Row, Col, ListGroup, Badge, Container, Button, Card } from 'react-bootstrap';
 import { FETCH_REQUEST, FETCH_SUCCESS, FETCH_FAIL } from '../../constants/actionTypes.js';
 import Rating from './Product/Rating.js';
+import {Helmet} from 'react-helmet-async'
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -44,50 +45,78 @@ function ProductScreen() {
     fetchData();
   }, [slug]);
 
-  return (
-    <Container>
-    loading? <>loading...</>
-    : error? <>{error}</>
-    :
+  return loading ? (
+    <>loading...</>
+    ) : error ? (
+    <>{error}</>
+    ) : (
+      <Container>
     <>
       <Row>
-          <Col md={6}>
-            <img 
-              src={product.image}
-              alt={product.name}
-            />
-          </Col>
-          <Col md={3}>
-            <ListGroup variant='flush'>
-              <ListGroup.Item>
-                <h1>{product.name}</h1>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <h1>Description: {product.description}</h1>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Rating 
-                  rating={product.rating}
-                  numReviews={product.numReviews}
-                />
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row> 
-                <Col>Status:</Col>
+        <Col md={6}>
+          <img
+            className="img-large"
+            src={product.image}
+            alt={product.name}
+          ></img>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <Helmet>
+                <title>KickBacks: {product.name}</title>
+              </Helmet>
+              <h1>{product.name}</h1>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                rating={product.rating}
+                numReviews={product.numReviews}
+              ></Rating>
+            </ListGroup.Item>
+            <ListGroup.Item>Price : ${product.price}</ListGroup.Item>
+            <ListGroup.Item>
+              Description:
+              <p>{product.description}</p>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <Card.Body>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>${product.price}</Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
                     <Col>
                       {product.countInStock > 0 ? (
                         <Badge bg="success">In Stock</Badge>
                       ) : (
-                        <Badge bg="danger">Out of Stock</Badge>
+                        <Badge bg="danger">Unavailable</Badge>
                       )}
                     </Col>
-                </Row>
-              </ListGroup.Item>
-            </ListGroup>
-          </Col>
-          <Col md={3}></Col>
-        </Row>    
-    </>
+                  </Row>
+                </ListGroup.Item>
+
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <div className="d-grid">
+                      <Button variant="primary">Add to Cart</Button>
+                    </div>
+                  </ListGroup.Item>
+                )}
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>)
     </Container>
   )
 }
