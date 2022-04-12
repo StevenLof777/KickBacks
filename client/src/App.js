47bf83b9c57314640a48981616d1ab5beca07f4c
Jake Finn <lof.steven@gmail.com>
2022-04-11 20:49:30 -0400

    Resolve fetch error

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import './index.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Dashboard from "./pages/Profile/Dashboard";
+// import Dashboard from "./pages/Profile/Dashboard";
 // import Login from "./pages/Login/Login";
 import Navbar from "./pages/Profile/Navbar.js";
-import Signup from "./pages/Signup/Signup";
+// import Signup from "./pages/Signup/Signup";
 import Home from './pages/Home/Home.js';
-import Products from './components/Products/Products.jsx'
+import Products from './components/Products/Products.js'
 
 function App() {
   return (
     <BrowserRouter>
     <Navbar/>
+
       <Routes>
-        {/* <Route exact path="/" element={<Login/>}/> */}
-        <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/product/:slug" element={<Products/>}/>
-          
-        <Route path="/signup" element={<Signup/>}/>
-          
-        <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/product/:slug" element={<Products />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
             
-      </Routes>
+
     </BrowserRouter>
   );
 }
