47959edc249e7193910f26b3cd64c6da6de12986
Jake Finn <lof.steven@gmail.com>
2022-04-12 20:31:44 -0400

    Connect to mongodb

--- server/server.js
+++ server/server.js
@@ -1,5 +1,16 @@
 import express from 'express';
 import data from './data.js';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+  console.log('DB connected')
+}).catch((err) =>{
+  console.log(err.message);
+})
+
 const app = express();
 
 app.get('/api/products', (req, res) => {
